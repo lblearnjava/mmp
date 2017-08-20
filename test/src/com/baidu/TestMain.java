e5a9769b0152362d6affa0ed80fbd2f6e2553ae3
lanbingdemayun <>
2017-08-20 17:32:16 +0800

    sdfsfs

--- test/src/com/baidu/TestMain.java
+++ test/src/com/baidu/TestMain.java
@@ -6,5 +6,6 @@ package com.baidu;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("jdfsdfsf");
     }
 }
--- test/src/com/baidu/fsdfs.java
+++ test/src/com/baidu/fsdfs.java
@@ -6,6 +6,7 @@ package com.baidu;
 public class fsdfs {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("fsdfsfd");
     }
 
 

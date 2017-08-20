5512eb4d5b124ae3d595897e8048654526b7b2f3
lanbingdemayun <>
2017-08-20 17:30:39 +0800

    dfsdfs

---
+++ test/src/com/baidu/TestMain.java
@@ -0,0 +1,10 @@
+package com.baidu;
+
+/**
+ * Created by lanbingdepingguo on 2017/8/20.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
---
+++ test/src/com/baidu/fsdfs.java
@@ -0,0 +1,13 @@
+package com.baidu;
+
+/**
+ * Created by lanbingdepingguo on 2017/8/20.
+ */
+public class fsdfs {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+
+
+
+    }
